c22837d3b1b75ca61e6b55110a1699bda666f4ab
JeffJacobson <JeffJacobson@users.noreply.github.com>
2014-07-29 09:59:57 -0700

    Updated to use new USGS elevation service.

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,41 +1,70 @@
-/*global require, esri*/
+/*global require*/
 /*jslint browser:true, debug: true*/
-require(["usgs", "dojo/on", "dojo/query", "dojo/NodeList-manipulate", "esri"], function (usgs, on, query) {
+require([
+	"usgs", "esri/config", "esri/map", "esri/geometry/webMercatorUtils", "esri/graphic", "esri/InfoTemplate", "dojo/Deferred"
+], function (usgs, esriConfig, Map, webMercatorUtils, Graphic, InfoTemplate, Deferred) {
 	"use strict";
-	esri.config.defaults.io.proxyUrl = "proxy.ashx"; // Set the proxy page. For more info see http://help.arcgis.com/en/webapi/javascript/arcgis/help/jshelp_start.htm#jshelp/ags_proxy.htm.
-	esri.config.defaults.io.timeout = 5000;
 
-	// Create the elevation task;
-	var elevationTask = new usgs.ElevationTask({
-		elevationOnly: false //true
+	esriConfig.defaults.io.proxyUrl = "proxy.ashx"; // Set the proxy page. For more info see http://help.arcgis.com/en/webapi/javascript/arcgis/help/jshelp_start.htm#jshelp/ags_proxy.htm.
+
+	var map = new Map("map", {
+		basemap: "streets",
+		center: [-120.80566406246835, 47.41322033015946],
+		zoom: 7,
+		showAttribution: true
 	});
 
-	on(elevationTask, "elevationReturned", function (response) { //elevation, units, dataSource, dataSource, queryPoint
-		query("#progress").remove();
-		if (typeof (response) === "number") {
-			query("body").append(["<p>Elevation = ", response, "</p>"].join(""));
-		} else {
-			(function () {
-				var list = query("body").append("<dl>");
-				list.append("<dt>Elevation</dt>").append(["<dd>", response.elevation, " ", response.units, "</dd>"].join(""));
-				list.append("<dt>Data Source</dt>").append(["<dd>", response.dataSource, "</dd>"].join(""));
-				list.append("<dt>Data ID</dt>").append(["<dd>", response.dataId, "</dd>"].join(""));
-				list.append("<dt>Query Point</dt>").append(["<dd>", response.queryPoint.x, ",", response.queryPoint.y].join(""));
-			} ());
-		}
-	});
-	on(elevationTask, "error", function (error) {
-		query("#progress").remove();
-		query("body").append("<p>An error occured: " + error.message + "</p>");
-		if (window.console) {
-			if (window.console.error) {
-				window.console.error(error);
+	//function createTitle(graphic) {
+	//	return ["Elevation at ", graphic.x, ",", graphic.y].join();
+	//}
+
+	function createContent(graphic) {
+		var deferred = new Deferred(), mapPoint;
+		mapPoint = webMercatorUtils.webMercatorToGeographic(graphic.geometry);
+		usgs.getElevation(mapPoint.x, mapPoint.y).then(function (elevationQueryResult) {
+			var output = ["<dl>"];
+			for (var propName in elevationQueryResult) {
+				if (elevationQueryResult.hasOwnProperty(propName)) {
+					output.push("<dt>", propName, "</dt>", "<dd>", elevationQueryResult[propName], "</dd>");
+				}
+
 			}
-		}
-	});
+			output.push("</dl>");
+			deferred.resolve(output.join(""));
+		});
 
-	elevationTask.getElevation({
-		x: -122.894783019987,
-		y: 47.0029095065612
+		return deferred;
+	}
+
+	var infoTemplate = new InfoTemplate("Elevation", createContent);
+
+
+	//usgs.getElevation(-122.894783019987, 47.0029095065612).then(function () {
+	//	console.log(arguments[0]);
+	//});
+
+	/**
+	 * @param {Event} e
+	 * @param {esri/geometry/Point} e.mapPoint - A point in the map coordinate system (web mercator).
+	 */
+	map.on("click", function (e) {
+		// Get the clicked map point.
+		var mapPoint = e.mapPoint;
+		var graphic = new Graphic(mapPoint);
+		graphic.setAttributes({});
+		graphic.setInfoTemplate(infoTemplate);
+		//var graphic = new Graphic(mapPoint);
+		//map.infoWindow.setContent(createContent(graphic));
+		map.infoWindow.show(mapPoint);
+		map.infoWindow.setContent("<progress>Getting Elevation from USGS...</progress>");
+		try {
+			// TODO: Figure out why this doesn't work.
+			map.infoWindow.setFeatures([graphic]);
+		} catch (err) {
+			console.log("Error attempting to set content.", err);
+			createContent(graphic).then(function (content) {
+				map.infoWindow.setContent(content);
+			});
+		}
 	});
 });
--- Scripts/usgs/main.js
+++ Scripts/usgs/main.js
@@ -1,125 +1,49 @@
-/*global define, esri*/
-/// <reference path="../jsapi_vsdoc_v32_2010.js" />
-
-// Request http://gisdata.usgs.gov/XMLWebServices2/Elevation_Service.asmx/getElevation?X_Value=-122.89478301998749&Y_Value=47.00290950656123&Elevation_Units=FEET&Source_Layer=*&Elevation_Only=1
-
-
-define(["dojo/Evented", "dojo/_base/declare", "dojo/query", "esri/utils", "esri/geometry"], function (Evented, declare, query) {
+/*global define*/
+define(["esri/request", "dojo/Deferred", "./ElevationQueryResult"], function (esriRequest, Deferred, ElevationQueryResult) {
 	"use strict";
-	var ElevationTask, ElevationTaskResponse;
 
-	// Declare the response class..
-	ElevationTaskResponse = declare(null, {
-		queryPoint: null,
-		dataSource: null,
-		dataId: null,
-		elevation: null,
-		units: null,
-		constructor: function (response) {
-			/// <summary>Creates a new instance of the ElevationTaskResponse class.</summary>
-			/// <param name="response" type="Object">Description</param>
-			var nodes = query("Elevation_Query", response);
-			this.queryPoint = new esri.geometry.Point(Number(nodes.attr("x")), Number(nodes.attr("y")), new esri.SpatialReference({ wkid: 4326 }));
-			this.dataSource = nodes.query("Data_Source")[0].textContent;
-			this.dataId = nodes.query("Data_ID")[0].textContent;
-			this.elevation = Number(nodes.query("Elevation")[0].textContent);
-			this.units = nodes.query("Units")[0].textContent;
+	/**
+	 * Queries the USGS Elevation service.
+	 * @param {number} x - WGS84 X coordinate
+	 * @param {number} y - WGS84 Y coordinate
+	 * @param {string} [units="Feet"] - Valid values are "Feet" and "Meters". Any value other than "Meters" (case-sensitive) will indicate "Feet".
+	 * @param {string} [url="http://ned.usgs.gov/epqs/pqs.php"]
+	 * @returns {dojo/Deferred}
+	 */
+	function getElevation(x, y, units, url) {
+		var deferred = new Deferred();
+		if (units !== "Meters") {
+			units = "Feet";
 		}
-	});
 
-	// Declare the task class...
-	ElevationTask = declare([Evented], {
-		serviceUrl: "http://gisdata.usgs.gov/XMLWebServices2/Elevation_Service.asmx",
-		sourceLayer: null,
-		constructor: function (args) {
-			// Override the default service url if one was provided.
-			if (!args) {
-				args = {};
-			}
-			this.serviceUrl = args.serviceUrl || "http://gisdata.usgs.gov/XMLWebServices2/Elevation_Service.asmx";
-			this.sourceLayer = args.sourceLayer || "*";
-			this.elevationUnits = args.elevationUnits || "FEET";
-			this.elevationOnly = args.elevationOnly || false;
-		},
-
-		getElevation: function (args) {
-			var self = this, x, y, elevationUnits, sourceLayer, elevationOnly, point, url = this.serviceUrl + "/getElevation";
-			if (args.point) {
-				// Project the point if explicitly set to non
-				if (args.point.spatialReference !== undefined && (args.point.spatialReference.wkid === 102100 || args.point.spatialReference.wkid === 3857)) {
-					point = esri.geometry.webMercatorToGeographic(point);
-					x = point.x;
-					y = point.y;
-				}
-			} else if (args.x !== undefined && args.y !== undefined) {
-				x = args.x;
-				y = args.y;
-			} else {
-				throw new Error("No input point was specified.  A either a point or an x and y value must be provided.");
-			}
-
-			sourceLayer = args.sourceLayer || this.sourceLayer || "*";
-			elevationUnits = args.elevationUnits || this.elevationUnits || "FEET";
-			elevationOnly = args.elevationOnly || this.elevationOnly ? "TRUE" : "FALSE";
-
-			return esri.request({
-				url: url,
-				content: {
-					X_Value: x,
-					Y_Value: y,
-					Elevation_Units: elevationUnits,
-					Source_Layer: sourceLayer,
-					Elevation_Only: elevationOnly
-				},
-				handleAs: "xml"
-			}, null).then(function (response) {
-				// Response (Elevation Only)
-				/*
-				<?xml version="1.0" encoding="utf-8"?>
-				<double>117.498733240163</double>
-				*/
-
-				// Full response
-				/*
-				<?xml version="1.0" encoding="utf-8"?>
-				<USGS_Elevation_Web_Service_Query>
-				<Elevation_Query x="-122.894783019987" y="47.0029095065612">
-				<Data_Source>NED 1/9th arc-second: Puget Sound Washington</Data_Source>
-				<Data_ID>Elev_WA_PugetSound</Data_ID>
-				<Elevation>117.498733240163</Elevation>
-				<Units>FEET</Units>
-				</Elevation_Query>
-				</USGS_Elevation_Web_Service_Query>
-				*/
-				var output;
-				// Convert response from XML to JavaScript object.
-				// Response will differ depending on the value of Elevation_Only in the request.
-				// First query for Elevation Only response, a single node called "double".
-
-				output = response;
-
-				if (response.firstChild.nodeName === "double") {
-					output = Number(response.firstChild.textContent);
-				} else if (response.firstChild.nodeName === "USGS_Elevation_Web_Service_Query") {
-					output = new ElevationTaskResponse(response);
-				} else {
-					output = response;
-				}
-
-				// Trigger the elevation returned event.  Consumed using dojo/on.
-				self.emit("elevationReturned", output);
-			}, function (error) {
-				// Trigger the error event.  Consumed using dojo/on.
-				self.emit("error", error);
-			});
-
-
-
+		// Ensure input parameters are numbers.
+		if (typeof x === "string") {
+			x = Number(x);
 		}
-	});
+		if (typeof y === "string") {
+			y = Number(y);
+		}
+
+		esriRequest({
+			url: url || "http://ned.usgs.gov/epqs/pqs.php",
+			content: {
+				x: x,
+				y: y,
+				units: units,
+				output: "json"
+			},
+			handleAs: "json"
+		}).then(function (result) {
+			var output = new ElevationQueryResult(result);
+			deferred.resolve(output);
+		}, function (error) {
+			deferred.reject(error);
+		});
+
+		return deferred;
+	}
 
 	return {
-		ElevationTask: ElevationTask,
-		ElevationTaskResponse: ElevationTaskResponse
+		getElevation: getElevation
 	};
 });
